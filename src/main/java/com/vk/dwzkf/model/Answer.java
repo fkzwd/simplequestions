89a2772f21f9829f66b7ca2681b05ab1c5743c3d
fkzwd <lives352@gmail.com>
2020-07-18 22:02:23 +0300

    Create model object -Answer-

---
+++ src/main/java/com/vk/dwzkf/model/Answer.java
@@ -0,0 +1,22 @@
+package com.vk.dwzkf.model;
+
+public class Answer {
+    private Integer id;
+    private String text;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
